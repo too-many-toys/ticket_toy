3f06debe19be1e2b450af82776ba74d2bb7fd3a9
mark <34611716+gmae199boy@users.noreply.github.com>
2023-10-23 23:21:27 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:scroll_loop_auto_scroll/scroll_loop_auto_scroll.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TOTDApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TOTDApp extends StatelessWidget {
+  const TOTDApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TOTDHomePage(title: '#TOTD | TICKET OF TODAY'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class TOTDHomePage extends StatefulWidget {
+  const TOTDHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TOTDHomePage> createState() => _TOTDHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _TOTDHomePageState extends State<TOTDHomePage> {
+  ScrollController controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title)),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            const Padding(padding: EdgeInsets.all(20)),
+            const SizedBox(
+              width: 500,
+              child: ScrollLoopAutoScroll(
+                duration: Duration(seconds: 100),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/1.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/2.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/3.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/4.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/5.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/6.jpg'),
+                      width: 200,
+                      height: 200,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(20)),
+            const Text('어떤 영화든 OK!'),
+            const Text('단 2-STEP 으로 뚝딱 만드는'),
+            const Text('디지털 오리지널 티켓'),
+            const Padding(padding: EdgeInsets.all(20)),
+            const Text('🎬 내가 본 영화는 오리지널 티켓이 안 나왔어요'),
+            const Padding(padding: EdgeInsets.all(5)),
+            const Text('🎟️ 갖고는 싶은데 수량이 적어서 너무 빨리 동나요'),
+            const Padding(padding: EdgeInsets.all(5)),
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+                '✍🏻 노션, 인스타, 블로그에 영화 리뷰를 작성할 때\n      오리지널 티켓 이미지를 첨부하고 싶어요'),
+            const Padding(padding: EdgeInsets.all(20)),
+            const Text('이 중 하나라도 해당된다면,'),
+            ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('You clicked ElevatedButton.')));
+                },
+                child: const Text('디지털 오리지널 티켓 만들기')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ticket_toy/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TOTDApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
